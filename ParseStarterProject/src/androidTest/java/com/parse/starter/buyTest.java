fccbdad911ef8ffb0c8d3f0ba526b7886957880e
kelsie1231 <kelsiedong@Kelsies-MacBook-Pro.local>
2015-12-03 22:30:53 -0800

    added login test, finished half buy test

---
+++ ParseStarterProject/src/androidTest/java/com/parse/starter/buyTest.java
@@ -0,0 +1,36 @@
+package com.parse.starter;
+
+import android.app.Activity;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseUser;
+import com.parse.starter.activity.BuyPageActivity;
+import com.parse.starter.activity.MainActivity;
+import com.parse.starter.adapter.RecyclerViewAdapter;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static junit.framework.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class buyTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule(MainActivity.class);
+    @Test
+    public void firstTest() throws Exception{
+        if (ParseUser.getCurrentUser() != null) ParseUser.logIn("test", "test");
+
+        onView(withId(R.id.buyButton)).perform(click());
+        onView(withId(R.id.buying_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+    }
+}
---
+++ ParseStarterProject/src/androidTest/java/com/parse/starter/loginTest.java
@@ -0,0 +1,33 @@
+package com.parse.starter;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseUser;
+import com.parse.starter.activity.LoginActivity;
+import com.parse.starter.activity.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static junit.framework.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class loginTest {
+    @Rule
+    public ActivityTestRule<LoginActivity> activityRule = new ActivityTestRule(LoginActivity.class);
+    @Test
+    public void firstTest(){
+        if (ParseUser.getCurrentUser() != null) ParseUser.logOut();
+
+        onView(withId(R.id.username)).perform(typeText("test"));
+        onView(withId(R.id.password)).perform(typeText("test"));
+        onView(withId(R.id.signin_button)).perform(click());
+
+    }
+}
